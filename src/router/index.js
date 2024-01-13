fe8cd8fd243b902c9fe4c1e121f7137661db4dc1
RIKO910 <tarikulislamriko910@gmail.com>
2024-01-14 01:23:33 +0600

    Auth,Layouts

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import DefaultLayoutVue from '@/Layouts/DefaultLayout.vue';
+import AuthLayoutVue from '@/Layouts/AuthLayout.vue';
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      redirect: '/dashboard',
+      component: DefaultLayoutVue,
+      meta:{requiresAuth: true},
+      children:[
+        {path:'/dashboard', name: 'dashboard', component:()=>import('@/views/Dashboard.vue')}
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/auth',
+      redirect: '/login',
+      component:AuthLayoutVue,
+      meta:{isGuest:true},
+      children: [
+        {path:'/login', name: 'login', component: ()=> import('@/views/Auth/Login.vue') }
+      ]
     }
   ]
 })
